24156ae803fd62133634fe93b31d33cfa571d8d1
Naeleno <naeleno@gmail.com>
2018-08-21 20:44:22 +0300

    add card to row layout

--- app/src/main/java/com/maximilian/wasit/wasitguide/ItemsListActivity.java
+++ app/src/main/java/com/maximilian/wasit/wasitguide/ItemsListActivity.java
@@ -5,11 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
